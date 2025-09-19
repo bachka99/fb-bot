9bc785afb609521d5eeeab33f5de48853c1bf22a
bachka99 <bahckabl7@gmail.com>
2025-09-19 07:01:11 +0200

    add send_message function

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
 VERIFY_TOKEN = "bachka_token"
+PAGE_ACCESS_TOKEN = "EAAZAmrUlytb0BPYg7ucghGA1pNTdrluj3QLKm924bF8yk7DMC02H9rWDRWD55Vl2aQNFNSec5iF7Y7XyPRXYnx58r1PU5gNJgCbejNy2XhWZBJYRa5eIZCFZA8TD4SytUum2bfAidLdgMmCdWZAJk2rGZBm11HZAMi20QqZAv4ksvDRE52Duhpe8tHsRK2MXVtK55uZBVJpqF"
 
 @app.route("/", methods=["GET"])
 def home():
@@ -21,4 +23,28 @@ def webhook():
     if request.method == "POST":
         data = request.json
         print("Message received:", data)
+
+        # Messenger-ээс ирсэн мессежийг шалгаж хариу өгөх
+        if "entry" in data:
+            for entry in data["entry"]:
+                if "messaging" in entry:
+                    for msg in entry["messaging"]:
+                        if "message" in msg:
+                            sender_id = msg["sender"]["id"]
+                            text = msg["message"].get("text")
+
+                            # Энгийн автомат хариу илгээх
+                            if text:
+                                send_message(sender_id, f"Чиний бичсэн: {text}")
+
         return "EVENT_RECEIVED", 200
+
+def send_message(recipient_id, message_text):
+    url = "https://graph.facebook.com/v23.0/me/messages"
+    params = {"access_token": PAGE_ACCESS_TOKEN}
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "recipient": {"id": recipient_id},
+        "message": {"text": message_text}
+    }
+    requests.post(url, params=params, headers=headers, json=data)
